90ec2fcd37048e1fc5c6b58e704f25ebbe8e4137
yeshwanth281297 <yesh281297@gmail.com>
2021-04-02 11:07:22 +0530

    ADDED:IS NOT NULL

---
+++ Isnotnull.py
@@ -0,0 +1,11 @@
+import mysql.connector as mysql
+#try:
+mydb = mysql.connect(user ="Yesh", password="Pass@123", host='127.0.0.1', database='college')
+mycursor = mydb.cursor()
+print(mycursor)
+mycursor.execute('SELECT name FROM college where age IS not NULL;')
+result=mycursor.fetchall()
+print(result)
+
+
+#mydb.commit()
