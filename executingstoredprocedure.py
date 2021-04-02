e75d0b378e486948f8a9c0435af2ecdbd048d359
yeshwanth281297 <yesh281297@gmail.com>
2021-04-02 11:03:34 +0530

    ADDED:Executing Stored Procedure

---
+++ executingstoredprocedure.py
@@ -0,0 +1,8 @@
+import mysql.connector as mysql
+#try:
+mydb = mysql.connect(user ="Yesh", password="Pass@123", host='127.0.0.1', database='college')
+mycursor = mydb.cursor()
+print(mycursor)
+mycursor.execute('EXEC proc1 ;')
+result=mycursor.fetchall()
+print(result)
