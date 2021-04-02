ceb0598cb5eae35e9ee0468e111c6a03423dcde2
yeshwanth281297 <yesh281297@gmail.com>
2021-04-02 11:12:12 +0530

    ADDED:Count Functions

---
+++ Countfunction.py
@@ -0,0 +1,11 @@
+import mysql.connector as mysql
+#try:
+mydb = mysql.connect(user ="Yesh", password="Pass@123", host='127.0.0.1', database='college')
+mycursor = mydb.cursor()
+print(mycursor)
+mycursor.execute('SELECT COUNT(age) FROM college;')
+result=mycursor.fetchall()
+print(result)
+
+
+#mydb.commit()
